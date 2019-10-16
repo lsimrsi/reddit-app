e9bb4a4189dc7d13f54aecd28e1d2a97fb76c400
Luke Simpson <luke.simpson@ruralsource.com>
2019-10-16 17:44:43 -0500

    match on status code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use actix_files as fs;
-use actix_web::{middleware, web, App, HttpResponse, HttpServer, Error};
+use actix_web::{middleware, web, App, Error, HttpResponse, HttpServer};
 use futures::Future;
 use reqwest::{self, Client};
-use serde::{Deserialize};
+use serde::Deserialize;
 // use std::path::PathBuf;
 
 static BASE_URL: &str = "http://www.reddit.com/r/";
@@ -12,20 +12,27 @@ static SLOWWLY: &str =
 
 // generic method for sending off a request (get or post)
 // and returns the response back to the front-end
-fn send_request(builder: reqwest::RequestBuilder) -> impl Future<Item = HttpResponse, Error = Error> {
+fn send_request(
+    builder: reqwest::RequestBuilder,
+) -> impl Future<Item = HttpResponse, Error = Error> {
     println!("Request sent:\n\r{:#?}", builder);
     actix_web::web::block(move || builder.send())
         .from_err()
-        .and_then(|mut res| match res.text() {
-            Ok(body) => HttpResponse::Ok()
-                .content_type("application/json")
-                .body(body),
-            Err(error) => {
-                println!("send_request error: {}", error);
-                HttpResponse::InternalServerError()
+        .and_then(|mut res| match res.status() {
+            reqwest::StatusCode::OK => match res.text() {
+                Ok(body) => HttpResponse::Ok()
                     .content_type("application/json")
-                    .body(format!("{{\"error\": \"Error getting response text.\"}}"))
-            }
+                    .body(body),
+                Err(error) => {
+                    println!("get text error: {}", error);
+                    HttpResponse::InternalServerError()
+                        .content_type("application/json")
+                        .body(format!("{{\"error\": \"Error getting response text.\"}}"))
+                }
+            },
+            _ => HttpResponse::InternalServerError()
+                .content_type("application/json")
+                .body(format!("{{\"error\": \"Couldn't get posts.\"}}")),
         })
 }
 
@@ -46,7 +53,7 @@ fn get_rust_posts_slowwly(
 // handles a variable in the requested path
 fn get_subreddit_data(
     subreddit: web::Path<(String)>,
-    client: web::Data<Client>
+    client: web::Data<Client>,
 ) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
     let subreddit = format!("{}{}{}", BASE_URL, subreddit, ".json");
     send_request(client.get(&subreddit))
@@ -61,7 +68,7 @@ struct SubredditQuery {
 // handles post request with a json body
 fn post_subreddit_data(
     srquery: web::Json<(SubredditQuery)>,
-    client: web::Data<Client>
+    client: web::Data<Client>,
 ) -> impl Future<Item = HttpResponse, Error = actix_web::Error> {
     let subreddit = format!("{}{}{}", BASE_URL, srquery.subreddit, ".json");
     send_request(client.post(&subreddit))
@@ -73,8 +80,13 @@ fn main() {
             .data(Client::new())
             .wrap(middleware::Logger::default())
             .service(web::resource("/rust/posts").route(web::get().to_async(get_rust_posts)))
-            .service(web::resource("/subreddit/data/{sr}").route(web::get().to_async(get_subreddit_data)))
-            .service(web::resource("/subreddit/data/").route(web::post().to_async(post_subreddit_data)))
+            .service(
+                web::resource("/subreddit/data/{sr}")
+                    .route(web::get().to_async(get_subreddit_data)),
+            )
+            .service(
+                web::resource("/subreddit/data/").route(web::post().to_async(post_subreddit_data)),
+            )
             .service(
                 web::resource("/get/rust/posts/slowwly")
                     .route(web::get().to_async(get_rust_posts_slowwly)),

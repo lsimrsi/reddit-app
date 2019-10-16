4b750c77da3f3ad9d3b8a2e166ef33afe9c09e86
Luke Simpson <luke.simpson@ruralsource.com>
2019-10-16 17:11:24 -0500

    fix className

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -87,7 +87,7 @@ function Post(props) {
 
 const Spinner = () =>
   <div className="spinner">
-    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+    <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
   </div>;
 
 export default App;
